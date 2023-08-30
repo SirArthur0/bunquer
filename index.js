c332a47a257622195cee6cd7dfcc25430efa8a84
SirArthur0 <arthurlmshell@gmail.com>
2023-08-30 06:36:30 -0300

    Pronto para inicializar users.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('express')()
 const consign = require('consign')
+const db = require('./config/db')
 
 
+app.db = db
 
 app.listen(5000, () =>  {
     console.log("Executando...")
